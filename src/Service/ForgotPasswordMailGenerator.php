3a449574fa40669d2ab639bb1e7f9e7fdf21982d
NichoSeb2 <NichoSeb2@outlook.fr>
2022-01-13 15:56:08 +0100

    fixed code duplication

--- src/Service/ForgotPasswordMailGenerator.php
+++ src/Service/ForgotPasswordMailGenerator.php
@@ -3,23 +3,23 @@
 namespace App\Service;
 
 use App\Entity\User;
+use App\Service\MailGenerator;
 use Symfony\Component\Mime\Address;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 
-class ForgotPasswordMailGenerator {
+class ForgotPasswordMailGenerator extends MailGenerator {
     public function getForgotPasswordMail(User $user, string $resetPasswordLink): TemplatedEmail {
 		$subject = 'Réinitialisation de votre mot de passe';
 
-        return (new TemplatedEmail())
-			->from(new Address($_SERVER['MAILER_NO_REPLY_EMAIL'], "SnowTricks"))
-			->to(new Address($user->getEmail(), $user->getUserIdentifier()))
-			->subject($subject)
-			->text("Bonjour, pour réinitialiser votre mot de passe merci de suivre le lien ci-dessous :\n\n". $resetPasswordLink)
-			->htmlTemplate('emails/password_reset.html.twig')
-			->context([
+		return $this->generateMail(
+			new Address($_SERVER['MAILER_NO_REPLY_EMAIL'], "SnowTricks"), 
+			new Address($user->getEmail(), $user->getUserIdentifier()), 
+			$subject, 
+			"Bonjour, pour réinitialiser votre mot de passe merci de suivre le lien ci-dessous :\n\n". $resetPasswordLink, 
+			"password_reset", [
 				'subject' => $subject, 
 				'link' => $resetPasswordLink
-			])
-		;
+			]
+		);
     }
 }
